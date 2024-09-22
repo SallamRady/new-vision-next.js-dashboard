2d85a50fb6d9bbd5efeeb08cf1273b4a555c298d
Sallam Rady <sallamrady@gmail.com>
2024-09-22 12:13:32 +0300

    implement and set main structure of user admin page

---
+++ src/app/(dashboard)/users/system-admin/page.tsx
@@ -0,0 +1,5 @@
+import UsersSystemAdminIndex from '@/views/pages/users/pages/system-admin'
+
+export default function UsersSystemAdminPage() {
+  return <UsersSystemAdminIndex />
+}
---
+++ src/views/pages/users/pages/system-admin/index.tsx
@@ -0,0 +1,10 @@
+import { Stack } from '@mui/material'
+import UsersSystemAdminTopCards from './components/top-cards'
+
+export default function UsersSystemAdminIndex() {
+  return (
+    <Stack spacing={3}>
+      <UsersSystemAdminTopCards />
+    </Stack>
+  )
+}
