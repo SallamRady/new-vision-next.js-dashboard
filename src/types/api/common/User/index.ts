07db17e1bd6abe07c9e084b29a10a6e60d7855ab
rustedmiind <alisolimanworks@gmail.com>
2024-10-08 15:41:03 +0300

    types - user, media

---
+++ src/types/api/common/User/index.ts
@@ -0,0 +1,28 @@
+import { Media } from '../Media'
+
+export interface User {
+  id: number
+  name: string
+  global_id: number
+  email: string
+  email_verified_at: any
+  created_at: string
+  updated_at: string
+  user_type_id: number
+  phone: string
+  status: number
+  identity: string
+  otp: any
+  otp_expire: any
+  login_way_id: number
+  can_set_pass: number
+  passport: string
+  iqama: any
+  border_number: any
+  country_id: number
+  pictures: Pictures
+}
+
+interface Pictures {
+  image: Media[]
+}
