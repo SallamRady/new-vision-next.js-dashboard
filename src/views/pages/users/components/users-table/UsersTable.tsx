e08d533f5a5c6a7c060ed6c3045a942369d2a098
Sallam Rady <sallamrady@gmail.com>
2024-10-08 23:34:46 +0300

    fetch users data

--- src/views/pages/users/components/users-table/UsersTable.tsx
+++ src/views/pages/users/components/users-table/UsersTable.tsx
@@ -1,7 +1,7 @@
 'use client'
 // import packages
-import { UsersTableRowType } from '@/types/users/users-page-types'
-import { Checkbox, Chip, IconButton, Typography } from '@mui/material'
+import { UserType } from '@/types/users/users-page-types'
+import { Checkbox, Chip, IconButton, Stack, Typography } from '@mui/material'
 import { createColumnHelper } from '@tanstack/react-table'
 import type { ColumnDef } from '@tanstack/react-table'
 
@@ -10,187 +10,18 @@ import { useMemo, useState } from 'react'
 import GenericDataTable from '@/components/tables/GenericDataTable'
 import AddUserDialogContent from './components/AddUser'
 import useUsersData from '@/hooks/useUsersData'
+import Loader from '@/components/Loader'
 
 // define column helper that will help to create tanstack table columns
-const columnHelper = createColumnHelper<UsersTableRowType>()
-
-const mockData: UsersTableRowType[] = [
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'john@example.com',
-    title: 'Software Engineer',
-    phone: '123-456-7890',
-    branch: 'Finance',
-    company: 'ABC Corp',
-    userType: 'Admin',
-    completionStatus: false
-  },
-  {
-    id: 2,
-    name: 'Jane Smith',
-    email: 'jane@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 3,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 4,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 5,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 6,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 7,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 8,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 9,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 10,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 11,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 12,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 13,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 14,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  },
-  {
-    id: 15,
-    name: 'User3',
-    email: 'User3@example.com',
-    title: 'Software Engineer',
-    phone: '987-654-3210',
-    branch: 'HR',
-    company: 'XYZ Inc',
-    userType: 'Employee',
-    completionStatus: true
-  }
-]
+const columnHelper = createColumnHelper<UserType>()
 
 export default function UsersDataTable() {
   // ** declare and define component state and variables
   const { data, isLoading, isError } = useUsersData()
-  console.log('Datttttttaaa ::', data, isLoading, isError)
-  // const [data, setData] = useState<UsersTableRowType[]>(mockData)
   //UserType
 
   // declare tanstack table columns
-  const columns = useMemo<ColumnDef<UsersTableRowType, any>[]>(
+  const columns = useMemo<ColumnDef<UserType, any>[]>(
     () => [
       {
         id: 'id',
@@ -219,13 +50,12 @@ export default function UsersDataTable() {
         cell: ({ row }) => <Typography color='text.primary'>{row.original.name}</Typography>,
         enableHiding: true // Allow hiding this column
       }),
-      columnHelper.accessor('userType', {
-        header: 'نوع المستخدم',
-        cell: ({ row }) => (
-          <Typography color='text.primary'>{row.original.userType == 'Admin' ? 'مستخدم' : 'موظف'}</Typography>
-        ),
-        enableHiding: true // Allow hiding this column
-      }),
+      // optionals
+      {
+        id: 'title',
+        header: 'المسمى الوظيفي',
+        cell: ({ row }) => <Typography color='text.primary'>_</Typography>
+      },
       columnHelper.accessor('email', {
         header: 'البريد الالكتروني',
         cell: ({ row }) => <Typography color='text.primary'>{row.original.email}</Typography>,
@@ -236,20 +66,26 @@ export default function UsersDataTable() {
         cell: ({ row }) => <Typography color='text.primary'>{row.original.phone}</Typography>,
         enableHiding: true // Allow hiding this column
       }),
-      columnHelper.accessor('branch', {
+      {
+        id: 'branch',
         header: 'الفرع',
-        cell: ({ row }) => <Typography color='text.primary'>{row.original.branch}</Typography>,
-        enableHiding: true // Allow hiding this column
-      }),
-      columnHelper.accessor('company', {
+        cell: ({ row }) => <Typography color='text.primary'>_</Typography>
+      },
+      columnHelper.accessor('tenants', {
         header: 'الشركة',
-        cell: ({ row }) => <Typography color='text.primary'>{row.original.company}</Typography>,
+        cell: ({ row }) => {
+          return (
+            <Stack width={'100%'} direction={'row'} flexWrap={'wrap'} spacing={2}>
+              {row.original.tenants?.map(ele => <Chip key={ele.id} label={ele.name} color='default' variant='tonal' />)}
+            </Stack>
+          )
+        },
         enableHiding: true // Allow hiding this column
       }),
-      columnHelper.accessor('completionStatus', {
+      columnHelper.accessor('status', {
         header: 'حالة الموظف',
         cell: ({ row }) => {
-          if (row.original.completionStatus == true) return <Chip label='مكتمل' color='success' variant='tonal' />
+          if (row.original.status == true) return <Chip label='مكتمل' color='success' variant='tonal' />
           return <Chip label='غير مكتمل' color='warning' variant='tonal' />
         },
         enableHiding: true // Allow hiding this column
@@ -278,16 +114,20 @@ export default function UsersDataTable() {
   // ** declare and define component helper methods
 
   // ** return component ui
+  if (isLoading) return <Loader />
+
   return (
-    <GenericDataTable
-      data={mockData}
-      columns={columns}
-      addButtonLabel='أضافة مستخدم'
-      addDialogContent={<AddUserDialogContent />}
-      exportButtonLabel='تصدير'
-      globalFilterPlaceholder='بحث...'
-      onExport={() => console.log('Export users clicked')}
-    />
+    <>
+      <GenericDataTable
+        data={data || []}
+        columns={columns}
+        addButtonLabel='أضافة مستخدم'
+        addDialogContent={<AddUserDialogContent />}
+        exportButtonLabel='تصدير'
+        globalFilterPlaceholder='بحث...'
+        onExport={() => console.log('Export users clicked')}
+      />
+    </>
   )
 }
 
