3d61e5dc4ed5b0e03dabe26983ff5c8fa4b74f43
rustedmiind <alisolimanworks@gmail.com>
2024-10-10 13:06:56 +0300

    api-v2 imports

--- src/Constants/Api.ts
+++ src/Constants/Api.ts
@@ -1,5 +1,8 @@
 import { Domain } from './Domain'
+import { api as apiv2 } from './api-v2'
 
 export const Api = (path: string) => {
   return `${Domain('api/')}${path}`
 }
+
+export const api = apiv2
