14f0a890782e4dbed1c64bbab1ffa1e03ae2f048
Sallam Rady <sallamrady@gmail.com>
2024-10-10 08:39:29 +0300

    api file

--- src/Constants/api.ts
+++ src/Constants/api.ts
@@ -1,11 +1,5 @@
 import { Domain } from './Domain'
 
-export function api(strings: TemplateStringsArray, ...values: unknown[]): string {
-  // Join the strings and values to form the final path
-  const path = strings.reduce((result, string, i) => {
-    return result + string + (values[i] || '')
-  }, '')
-
-  const api = `${Domain()}api/`
-  return api + path
+export const Api = (path: string) => {
+  return `${Domain('api/')}${path}`
 }
