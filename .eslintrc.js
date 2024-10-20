3218ff7634058685770563f280ed8a9203279480
rustedmiind <alisolimanworks@gmail.com>
2024-10-20 19:50:43 +0300

    ..

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'plugin:import/recommended', 'prettier'],
   rules: {
+    'react-hooks/exhaustive-deps': 'off',
     'jsx-a11y/alt-text': 'off',
     'react/display-name': 'off',
     'react/no-children-prop': 'off',
