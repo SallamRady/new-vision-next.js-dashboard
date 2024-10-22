53b86a64db54a2354abfd148068adf34eb5dfe58
SallamRady <sallamrady@gmail.com>
2024-10-22 10:20:51 +0300

    implement company packages card

--- src/views/pages/company-profile/index.tsx
+++ src/views/pages/company-profile/index.tsx
@@ -1,11 +1,23 @@
-import { Stack } from '@mui/material'
+import { Grid, Stack } from '@mui/material'
 
 import CompanyProfileHeader from './components/top-card'
+import CompanyPackageCard from './components/package-card'
 
 export default function CompanyDetailsPage() {
   return (
     <Stack spacing={2}>
+      {/* header */}
       <CompanyProfileHeader />
+      <Grid container gap={3}>
+        {/* package card */}
+        <Grid xs={12} md={4}>
+          <CompanyPackageCard />
+        </Grid>
+        {/* Employees */}
+        <Grid xs={12} md={7}>
+          Employees
+        </Grid>
+      </Grid>
     </Stack>
   )
 }
